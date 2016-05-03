a48850a0ac6a4aee6b276366b06c7b00884e654b
Hangchen Yu <yuhc123@gmail.com>
2016-05-03 14:43:40 -0500

    [theme] Add Numix theme for Ubuntu 14.04

---
+++ numix-theme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Work under Ubuntu 14.04"
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install numix-gtk-theme numix-icon-theme-circle
+sudo apt-get install numix-wallpaper-notd
+
